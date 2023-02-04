f908831ffd171e15aa56baf81af4360bc9b8bdbe
Haseeb Yousuf <haseebyousuff@gmail.com>
2023-02-04 23:29:26 +0530

    feature: add and delete task

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,92 @@
 import { useState } from "react";
-
+import { GiHornedHelm } from "react-icons/gi";
+import { AiOutlinePlus } from "react-icons/ai";
 function App() {
-    const [count, setCount] = useState(0);
+    const [tasks, setTasks] = useState([]);
+    const [task, setTask] = useState("");
 
+    //add task
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newTask = {
+            id: Math.floor(Math.random() * 1000),
+            taskName: task,
+            completed: false,
+        };
+        setTasks([...tasks, newTask]);
+        setTask("");
+    };
+
+    //delete task
+    const deleteTask = (id) => {
+        let fiteredTasks = [...tasks].filter((task) => task.id !== id);
+        setTasks(fiteredTasks);
+        console.log("Deleted");
+    };
+
+    //toggle completed
+
+    const toggleComplete = (id) => {};
+    const date = new Date();
+    const days = [
+        "Sunday",
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+    ];
+    const months = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
     return (
-        <div className="App">
-            <h1>Hello</h1>
+        <div className="app">
+            <div className="container">
+                <h1>
+                    <GiHornedHelm />
+                    Powerlist
+                </h1>
+                <div className="date">
+                    <p>{days[date.getDay()]}</p>
+                    <p>{date.getDate()},</p>
+                    <p>{months[date.getMonth()]}</p>
+                    <p>{date.getFullYear()}</p>
+                </div>
+                <form onSubmit={handleSubmit}>
+                    <AiOutlinePlus />
+                    <input
+                        value={task}
+                        onChange={(e) => setTask(e.target.value)}
+                        type="text"
+                        placeholder="Add a Task"
+                    />
+                </form>
+
+                <div>
+                    {tasks.map((task) => (
+                        <div key={task.id}>
+                            <p>
+                                {task.taskName}
+                                <button onClick={() => deleteTask(task.id)}>
+                                    Delete
+                                </button>
+                            </p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./app.css";
+import "./app.scss";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
